eb3fe71de2a3e98233a00f38069a8fd7010674be
Arda Balkan <ardabalkan@gmail.com>
2023-08-20 19:40:27 +0300

    Initial commit

---
+++ js_result.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_GET['jsonData'])) {
+  // get and decode submitted JSON data with a little safety check
+  $jsonData = json_decode(strip_tags($_GET['jsonData']), true);
+  echo "<h2>Submitted JSON Data</h2>";
+  echo "<ul>";
+  foreach ($jsonData as $key => $data) {
+    echo "<li>$data</li>";
+  }
+  echo "</ul>";
+  echo ($key + 1)." rows received.";
+}
+?>
