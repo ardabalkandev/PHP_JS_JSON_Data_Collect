6b48da90fbf802f83b3dcd59697feb5a8028777a
Ali Osman Yüksel <aliosmanyuksel@outlook.com>
2023-08-20 20:58:10 +0300

    Update Form functionality

--- js_result.php
+++ js_result.php
@@ -1,13 +1,22 @@
 <?php
 if(isset($_GET['jsonData'])) {
-  // get and decode submitted JSON data with a little safety check
+  // get and decode submitted JSON data with proper safety checks
   $jsonData = json_decode($_GET['jsonData'], true);
-  echo "<h2>Submitted JSON Data</h2>";
-  echo "<ul>";
-  foreach ($jsonData as $key => $data) {
-    echo "<li>$data</li>";
+
+  if ($jsonData !== null) {
+    echo "<h2>Submitted JSON Data</h2>";
+    echo "<ul>";
+    foreach ($jsonData as $data) {
+      echo "<li>" . htmlspecialchars($data) . "</li>";
+    }
+    echo "</ul>";
+    echo count($jsonData) . " rows received.";
+  } else {
+    echo "<h2>Error</h2>";
+    echo "Invalid JSON data received.";
   }
-  echo "</ul>";
-  echo ($key + 1)." rows received.";
+} else {
+  echo "<h2>Error</h2>";
+  echo "No JSON data received.";
 }
 ?>
