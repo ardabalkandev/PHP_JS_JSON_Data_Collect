e18fc28eede54c8903a4e1732631fb97d4fc8da0
Arda Balkan <ardabalkan@gmail.com>
2023-08-20 20:15:15 +0300

    Update js_result.php

--- js_result.php
+++ js_result.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_GET['jsonData'])) {
   // get and decode submitted JSON data with a little safety check
-  $jsonData = json_decode($_GET['jsonData']), true);
+  $jsonData = json_decode($_GET['jsonData'], true);
   echo "<h2>Submitted JSON Data</h2>";
   echo "<ul>";
   foreach ($jsonData as $key => $data) {
